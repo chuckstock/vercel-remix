9c5c663af2c26e8634d719e04d0956bf3cbb82cb
Charlie Blackstock <charlieblackstock@gmail.com>
2023-04-24 13:45:33 -0600

    add laoder

--- app/root.tsx
+++ app/root.tsx
@@ -8,9 +8,10 @@ import {
 } from '@remix-run/react'
 import { Analytics } from '@vercel/analytics/react'
 
-// export const loader = async () => {
-//   return json({ ok: true })
-// }
+export const loader = async () => {
+  return json({ ok: true })
+}
+
 export default function App() {
   return (
     <html lang='en'>

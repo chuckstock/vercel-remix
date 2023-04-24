25fea7a21063662938d5fb5f0945746d8bb9bcca
Charlie Blackstock <charlieblackstock@gmail.com>
2023-04-24 13:27:57 -0600

    fix

--- remix.config.js
+++ remix.config.js
@@ -1,13 +1,27 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
+const { flatRoutes } = require('remix-flat-routes')
+
+/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ['**/.*'],
+  // When running locally in development mode, we use the built in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  ignoredRouteFiles: ['**/*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
+  // serverBuildPath: "api/index.js",
   // publicPath: "/build/",
+  routes: async (defineRoutes) => {
+    return flatRoutes('routes', defineRoutes, {
+      ignoredRouteFiles: [
+        '.*',
+        '**/*.css',
+        '**/*.test.{js,jsx,ts,tsx}',
+        '**/__*.*',
+      ],
+    })
+  },
   future: {
-    v2_routeConvention: true,
+    v2_meta: true,
+    unstable_cssSideEffectImports: true,
   },
 }

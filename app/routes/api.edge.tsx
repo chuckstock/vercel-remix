011ebbd41e77ed1921af0038ee01fcb4d71ee4c5
Charlie Blackstock <charlieblackstock@gmail.com>
2023-04-25 08:55:07 -0600

    tes

--- app/routes/api.edge.tsx
+++ app/routes/api.edge.tsx
@@ -1,9 +1,7 @@
+import { json } from '@vercel/remix'
+
 export const config = { runtime: 'edge' }
 
-export default function Edge() {
-  return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix@Edge</h1>
-    </div>
-  )
+export async function loader() {
+  return json({ ok: true })
 }

3a9eba4bb46a2e769607ee444b81394973b7bf05
Charlie Blackstock <charlieblackstock@gmail.com>
2023-04-30 15:04:20 -0600

    test

--- apps/remix/remix.config.js
+++ apps/remix/remix.config.js
@@ -3,16 +3,16 @@ const { flatRoutes } = require('remix-flat-routes')
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
-  // routes: async (defineRoutes) => {
-  //   return flatRoutes('routes', defineRoutes, {
-  //     ignoredRouteFiles: [
-  //       '.*',
-  //       '**/*.css',
-  //       '**/*.test.{js,jsx,ts,tsx}',
-  //       '**/__*.*',
-  //     ],
-  //   })
-  // },
+  routes: async (defineRoutes) => {
+    return flatRoutes('routes', defineRoutes, {
+      ignoredRouteFiles: [
+        '.*',
+        '**/*.css',
+        '**/*.test.{js,jsx,ts,tsx}',
+        '**/__*.*',
+      ],
+    })
+  },
   future: {
     v2_meta: true,
     unstable_cssSideEffectImports: true,

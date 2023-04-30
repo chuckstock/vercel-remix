ee09ec549a7715bad8d47a014ea1f2b816ebba16
Charlie Blackstock <charlieblackstock@gmail.com>
2023-04-30 14:50:56 -0600

    try flat routes

--- apps/remix/app/routes/resources+/edge.ts
+++
@@ -1,7 +0,0 @@
-import { json } from '@vercel/remix'
-
-export const config = { runtime: 'edge' }
-
-export async function loader() {
-  return json({ ok: true })
-}
--- apps/remix/remix.config.js
+++ apps/remix/remix.config.js
@@ -2,24 +2,17 @@ const { flatRoutes } = require('remix-flat-routes')
 
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
   ignoredRouteFiles: ['**/*'],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "api/index.js",
-  // publicPath: "/build/",
-  // routes: async (defineRoutes) => {
-  //   return flatRoutes('routes', defineRoutes, {
-  //     ignoredRouteFiles: [
-  //       '.*',
-  //       '**/*.css',
-  //       '**/*.test.{js,jsx,ts,tsx}',
-  //       '**/__*.*',
-  //     ],
-  //   })
-  // },
+  routes: async (defineRoutes) => {
+    return flatRoutes('routes', defineRoutes, {
+      ignoredRouteFiles: [
+        '.*',
+        '**/*.css',
+        '**/*.test.{js,jsx,ts,tsx}',
+        '**/__*.*',
+      ],
+    })
+  },
   future: {
     v2_meta: true,
     unstable_cssSideEffectImports: true,

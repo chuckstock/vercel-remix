60b59af372106048adf202b8dbfdc38cf741458d
Charlie Blackstock <charlieblackstock@gmail.com>
2023-04-30 14:57:19 -0600

    routes

--- apps/remix/app/routes/_app.edge.tsx
+++
@@ -1,7 +0,0 @@
-export default function Edge() {
-  return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix@Edge</h1>
-    </div>
-  )
-}
--- apps/remix/app/routes/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from '@remix-run/react'
-import { json } from '@vercel/remix'
-export const loader = async () => {
-  return json({ ok: true })
-}
-
-export default function App() {
-  return <Outlet />
-}

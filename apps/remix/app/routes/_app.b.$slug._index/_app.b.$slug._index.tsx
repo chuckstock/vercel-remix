c02a5da155ee82df9bf329750cda973d61124b22
Charlie Blackstock <charlieblackstock@gmail.com>
2023-05-01 16:59:29 -0600

    i

--- apps/remix/app/routes/_app.b.$slug._index/_app.b.$slug._index.tsx
+++
@@ -1,7 +0,0 @@
-import type { LoaderArgs } from '@vercel/remix'
-import { redirect } from '@vercel/remix'
-
-export async function loader({ params, request }: LoaderArgs) {
-  const url = new URL(request.url)
-  throw redirect(`/b/${params.slug}/quests${url.search}`)
-}
--- apps/remix/app/routes/_app.b.$slug._index/index.tsx
+++ apps/remix/app/routes/_app.b.$slug._index/index.tsx
@@ -1,1 +1,7 @@
-export { default } from './_app.b.$slug._index';
+import type { LoaderArgs } from '@vercel/remix'
+import { redirect } from '@vercel/remix'
+
+export async function loader({ params, request }: LoaderArgs) {
+  const url = new URL(request.url)
+  throw redirect(`/b/${params.slug}/quests${url.search}`)
+}
